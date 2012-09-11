5a68952241ea0461113821f43c858046d8ffadc6
Paul Gregory <paul@monki>
2012-09-12 00:53:57 +0100

    style changes

--- includes/ContactUs.php
+++ includes/ContactUs.php
@@ -8,9 +8,10 @@ re-open on the 4<sup>th</sup> January at <em>09:00</em>.
 <br /><br />
 Any emails sent will be opened on our return.</p><br />-->
  
+<h1>Contact Us</h1>
+<div class="clearfix">
+<div class="column-1">
 <form id="Form" method="post" action="">
-	<fieldset>
-		<legend>Contact Us </legend>
 		<div id="contactDetails">				
 			<label>Company Name</label>
 				<input type="text" name="CompanyName" class="contactLine" value="" maxlength="255"/>
@@ -27,13 +28,16 @@ Any emails sent will be opened on our return.</p><br />-->
 				<textarea class="comments" name="Comments" rows="5" cols="25" onblur="required(this, 'text');"></textarea>
 				<span class="required" id="CommentsReq">*</span>
 		</div>
-	</fieldset>
-	<!--<a href="#" name="Submit" title="Send Message" class="submitButton" onclick="submitForm(); return false;">Send Message</a>-->
-		<input type="submit" class="submitButton" name="Submit" value="Submit" />
-</form>
 
+	<!--<a href="#" name="Submit" title="Send Message" class="submitButton" onclick="submitForm(); return false;">Send Message</a>-->
+	<div id="contactSubmit"><input type="submit" class="submitButton" name="Submit" value="Submit" style="margin-left: 165px" /></div>
+</form>
+</div>
+<div class="column-2">
 <div class="contact_address">
 <p>Please feel free to contact us at the address below, or send a comment in the form provided.</p>
 <p>DSG Auto Contracts Ltd<br />Camelot House<br />Bredbury Park Way<br />Bredbury<br />Cheshire<br />SK6 2SN</p>
 <p>Phone : <?php echo $CONTACT_NUMBER; ?><br />Fax : <?php echo $FAX_NUMBER; ?></p>
+</div>
+</div>
 </div>
