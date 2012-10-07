62c17e052ed989e77acb9623da051d649841b1a8
Paul Gregory <paul@monki>
2012-10-07 16:49:59 +0100

    Reverted the blog config file to the dsgauto.com settings

--- blog/wp-config.php
+++ blog/wp-config.php
@@ -19,13 +19,13 @@
 define('DB_NAME', 'dsgac_blog');
 
 /** MySQL database username */
-define('DB_USER', 'root');
+define('DB_USER', 'services');
 
 /** MySQL database password */
-define('DB_PASSWORD', 'root');
+define('DB_PASSWORD', 'd15hcl0th');
 
 /** MySQL hostname */
-define('DB_HOST', 'localhost');
+define('DB_HOST', 'databasehost.dsgfs.com');
 
 /** Database Charset to use in creating database tables. */
 define('DB_CHARSET', 'utf8');
