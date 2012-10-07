f072f34deff6e950ae8bd91c7d3f8825fbee07ae
Paul Gregory <paul@monki>
2012-10-07 16:26:14 +0100

    Added more helpful MSSQL failure messages.

--- includes/mssqlConnect.php
+++ includes/mssqlConnect.php
@@ -2,8 +2,8 @@
 
 // Connect to MSSQL server
 $con = mssql_connect(MSSQL_HOST,MSSQL_USER,MSSQL_PASS) 
-    or die('Could not connect to MS SQL server');
+    or die('Could not connect to MS SQL server. Error: '.mssql_get_last_message());
  
 // Select a database:
 mssql_select_db(MSSQL_NAME) 
-    or die('Could not select a database: '.MSSQL_NAME);
+    or die('Could not select a database. Error: '.mssql_get_last_message());
