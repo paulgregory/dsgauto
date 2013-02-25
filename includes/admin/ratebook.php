d5efcce3ccf6c1b8563b81e2361390e724336c55
Paul Gregory <paul@monki>
2013-02-25 23:02:18 +0000

    Yet more changes to the ratebook importer

--- includes/admin/ratebook.php
+++ includes/admin/ratebook.php
@@ -1,6 +1,5 @@
 <?php
 
-$fileUploaded = FALSE;
 $fileProcessed = FALSE;
 
 // Check if a given numeric field is a valid float or int and return NULL if not
@@ -17,26 +16,31 @@ if (isset($_SESSION['status']))
 {
 	if ($_SESSION['status'] == 'authenticated')
 	{
-		
+		// Process the form
 		if(isset($_POST['uploadSubmit']))
 		{
-						
+			// Throw an error if the upload failed
 			if ($_FILES["ratebookFile"]["error"] > 0) {
-			  $error = 'There was an error uploading the file.';
+			  $error = 'There was an error uploading the file.<br /><br /><em>Error number: '.$_FILES["ratebookFile"]["error"].'</em>';
 			}
 			else {
-			  if ($_FILES["ratebookFile"]['type'] !== 'text/csv' && $_FILES["ratebookFile"]['type'] !== 'text/plain') {
-				  $error = 'Please upload a suitable CSV file.';
+				// Check if the file is a known CSV format
+				$acceptable_types = array('text/csv', 'text/plain', 'application/vnd.ms-excel', 'application/octet-stream');
+			 	if (!in_array($_FILES["ratebookFile"]['type'], $acceptable_types)) {
+				  $error = 'Please upload a suitable CSV file.<br /><br /><em>Filename: '.$_FILES["ratebookFile"]['name'].'<br />File type: '.$_FILES["ratebookFile"]['type'].'</em>';
 			  }
 			  else {
+				  // Get a handle on the temporary file
 				  $handle = fopen($_FILES['ratebookFile']['tmp_name'], "r");
-				  $headers = fgetcsv($handle, 1, ",");
+				  
 			    // count and check the columns to see if we're dealing with the correct CSV file
+			    $headers = fgetcsv($handle, 1, ",");
 			    if (count($headers) != 30 || $headers[0] != 'CAP_Id' || $headers[6] != 'Variant' || $headers[9] != 'CO2' || $headers[17] != 'FinanceRental') {	
 					  $error = 'This CSV file doesn\'t contain the expected columns. Is this the correct Ratebook CSV?';
 					  break;
 				  }
 				  else {
+					
 					  // Empty the ratebook table before import
 					  mysql_query('TRUNCATE TABLE `tblratebook`');
 						if (mysql_error() != '') {				
@@ -44,15 +48,16 @@ if (isset($_SESSION['status']))
 							break;
 						}
 
-					  // Import the csv contents
+					  // Import the csv contents in one go - using MySQL 'LOAD DATA LOCAL INFILE'
 					  $linecount = 0;
-						$loadsql = 'LOAD DATA LOCAL INFILE "'.$_FILES['ratebookFile']['tmp_name'].'" INTO TABLE tblratebook FIELDS TERMINATED BY "," OPTIONALLY ENCLOSED BY """" LINES TERMINATED BY "\r" IGNORE 1 LINES';
+						$loadsql = 'LOAD DATA LOCAL INFILE "'.addslashes($_FILES['ratebookFile']['tmp_name']).'" INTO TABLE tblratebook FIELDS TERMINATED BY "," OPTIONALLY ENCLOSED BY """" LINES TERMINATED BY "\r" IGNORE 1 LINES';
 
 						if (!mysql_query($loadsql)) {
+							// The query has failed - throw an error
 							$error = 'There was an error importing the CSV ('.mysql_error().')';
 						}
 						else {
-							// Get the number of rows in the table after import
+							// The query suceeded - get the number of imported rows to display a message
 							$result = mysql_query('SELECT COUNT(*) FROM `tblratebook`');
 			        $row = mysql_fetch_array($result);
 			        $linecount = $row[0];
@@ -63,31 +68,6 @@ if (isset($_SESSION['status']))
 			}
 		}
 
-    if (isset($_POST['processSubmit'])) {
-	
-			// Empty the ratebook table before import
-		  mysql_query('TRUNCATE TABLE `tblratebook`');
-			if (mysql_error() != '') {				
-			  $error = 'Error whilst deleting existing content ('.mysql_error().')';
-				break;
-			}
-		
-		  // Import the csv contents
-		  $linecount = 0;
-			$loadsql = 'LOAD DATA LOCAL INFILE "'.$_SESSION['uploadedFile'].'" INTO TABLE tblratebook FIELDS TERMINATED BY "," OPTIONALLY ENCLOSED BY """" LINES TERMINATED BY "\r" IGNORE 1 LINES';
-
-			if (!mysql_query($loadsql)) {
-				$error = 'There was an error importing the CSV ('.mysql_error().')';
-			}
-			else {
-				// Get the number of rows in the table after import
-				$result = mysql_query('SELECT COUNT(*) FROM `tblratebook`');
-        $row = mysql_fetch_array($result);
-        $linecount = $row[0];
-				$fileProcessed = TRUE;
-			}
-
-	  }
 ?>
 <div id="administrationTop"></div>
 <div id="administration">
@@ -100,16 +80,6 @@ if (isset($_SESSION['status']))
 	  <h3>ERROR!</h3>
 	  <?php print '<p><strong>'.$error.'</strong></p>'; ?>
 	  <p><a href="/administration-ratebook.html">Try again</a></p>
-	
-	  <?php elseif ($fileUploaded): ?>
-
-    <h3>File Uploaded - Import Data?</h3> 
-
-    <p><strong>Filename: <?php print $_FILES["ratebookFile"]['name']; ?></strong></p>
-    <p>The CSV file has been successfully uploaded.</p>
-    <p><strong style="color: red">WARNING: This will delete all current ratebook data and replace it with the contents of this file. This might take a few moments...</strong></p>
-
-    <p><input type="submit" name="processSubmit" value="Import Data" onclick="$(this).val('...importing...');"> &nbsp;&nbsp;&nbsp;<a href="/administration.html">Cancel</a></p>
 
 	  <?php elseif ($fileProcessed): ?>
   
